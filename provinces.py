38ddcca10fea66ecd3181a46ac33d7bd6ec234eb
furkansina <63507255+furkansina@users.noreply.github.com>
2026-08-07 14:34:42 +0300

    Ikinci veri kaynagi eklendi (turkbusinesscenter.com is rehberi) + OSM'e tyres etiketi + genisletilmis rehber kategorileri (ayiklama skorlama sistemine birakildi)

--- provinces.py
+++ provinces.py
@@ -13,6 +13,22 @@ PROVINCES = [
     "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", "Düzce",
 ]
 
+# turkbusinesscenter.com'un il URL slug'ları - PROVINCES listesiyle TAM AYNI SIRADA (index'ler eşleşir).
+# Sitenin kendi il seçim menüsünden alındı, uydurma değil.
+PROVINCE_SLUGS_TBC = [
+    "adana", "adiyaman", "afyon", "agri", "amasya", "ankara", "antalya",
+    "artvin", "aydin", "balikesir", "bilecik", "bingol", "bitlis", "bolu", "burdur",
+    "bursa", "canakkale", "cankiri", "corum", "denizli", "diyarbakir", "edirne",
+    "elazig", "erzincan", "erzurum", "eskisehir", "gaziantep", "giresun", "gumushane",
+    "hakkari", "hatay", "isparta", "mersin", "istanbul", "izmir", "kars", "kastamonu",
+    "kayseri", "kirklareli", "kirsehir", "kocaeli", "konya", "kutahya", "malatya",
+    "manisa", "kahramanmaras", "mardin", "mugla", "mus", "nevsehir", "nigde", "ordu",
+    "rize", "sakarya", "samsun", "siirt", "sinop", "sivas", "tekirdag", "tokat",
+    "trabzon", "tunceli", "sanliurfa", "usak", "van", "yozgat", "zonguldak", "aksaray",
+    "bayburt", "karaman", "kirikkale", "batman", "sirnak", "bartin", "ardahan",
+    "igdir", "yalova", "karabuk", "kilis", "osmaniye", "duzce",
+]
+
 # Lead adı/isim eşleşmesi için sektör anahtar kelimeleri.
 # NOT: "toptan" (wholesale) kasıtlı olarak burada YOK - tek başına çok genel bir kelime,
 # market/tekstil/gıda gibi tamamen alakasız toptancıları da yakalayıp yanlış pozitif üretiyordu
